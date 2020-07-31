fee369a484fd6311e46df30e6b0c9b295e2e2b92
Cindy Cai <caicindy87@gmail.com>
2020-07-31 22:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 // Code your solution here!
 
+function printString(str) {
+  console.log(str[0])
+  if (str.length > 1) {
+    printString(str.substring(1))
+  } else {
+    return true;
+  }
+}
+
+
+function reverseString(str) {
+  if (str === "") {
+    return ""
+  } else {
+    return reverseString(str.substring(1)) + str[0]
+  }
+}
+
+
+function isPalindrome(str) {
+  if (str.length === 1 || str === "") return true;
+  
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.substring(1, str.length - 1))
+  } else {
+    return false
+  }
+  
+  return true;
+}
+
+// add up to and including the element at the given index
+function addUpTo(arr, index) {
+  
+}
